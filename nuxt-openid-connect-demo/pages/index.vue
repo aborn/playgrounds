262b4567ca6d679c72a93116f2c545b1d6a815a8
aborn <guobao.jiang@aliyun.com>
2022-06-10 18:26:26 +0800

    nuxt-openid-connect 0.1.2

--- nuxt-openid-connect-demo/pages/index.vue
+++ nuxt-openid-connect-demo/pages/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    Nuxt module playground!
+    <div>
+      Nuxt OpenID-Connect playground!
+    </div>
+    <p>isLoggedIn: {{ $oidc.isLoggedIn }}</p>
+    <p>user: {{ $oidc.user }}</p>
+    <button v-if="!$oidc.isLoggedIn" @click="$oidc.login()">
+      Login
+    </button>
+    <button v-else @click="$oidc.logout()">
+      Logout
+    </button>
   </div>
 </template>
 
 <script setup>
+// alternatively, you can also use it here
+const { $oidc } = useNuxtApp()
+// const user = useState('useState')
 </script>
