12dcb5c91cb989683ad12089f3094f55768416b0
aborn <guobao.jiang@aliyun.com>
2022-06-10 18:31:07 +0800

    config

--- nuxt-openid-connect-demo/nuxt.config.ts
+++ nuxt-openid-connect-demo/nuxt.config.ts
@@ -4,5 +4,25 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   modules: [
     'nuxt-openid-connect'
-],
+  ],
+  openidConnect: {
+    addPlugin: true,
+    op: {
+      issuer: 'your_oidc_provider_issuer',
+      clientId: 'your_client_id',
+      clientSecret: 'your_client_secret',
+      callbackUrl: 'http://localhost:3000/oidc/cbt',
+      scope: [
+        'email',
+        'profile',
+        'address'
+      ]
+    },
+    session: {
+      secret: 'oidcsessionid',
+      cookie: {},
+      resave: false,
+      saveUninitialized: false
+    }
+  }
 })
