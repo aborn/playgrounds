bf1b7023e1e883fcb858a861142ad84e98c2e70d
jiangguobao <jiangguobao@bixin.cn>
2022-07-22 19:52:16 +0800

    demo

---
+++ nuxt-openid-connect-demo/pages/demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ vMsg }}</p>
+    <button @click="onClickVMsg">MsgBtn</button>
+  </div>
+</template>
+
+<script setup>
+const vMsg = ref("hello msg")
+</script>
+
+<script>
+export default {
+  methods: {
+    onClickVMsg() {
+      console.log(this.vMsg)
+      this.vMsg = 'currentTime==>' + new Date()
+    }
+  }
+}
+</script>
